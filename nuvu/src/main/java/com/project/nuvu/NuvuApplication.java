4701f616f9d45961a7a11dd7d9ac5bfb10cb0a97
OrlandoRNX <orlandorivasrubio@live.com>
2021-02-05 23:22:09 -0500

    add initial project structure

---
+++ nuvu/src/main/java/com/project/nuvu/NuvuApplication.java
@@ -0,0 +1,13 @@
+package com.project.nuvu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuvuApplication.class, args);
+	}
+
+}
